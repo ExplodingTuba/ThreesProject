d13e36d1ba40bf3ad6122bf8e8bf649c4fe1dff1
Anthony B <anthonyrblanton@gmail.com>
2017-01-25 21:29:02 -0500

    Added methods that set and get the high score

--- FileIOExample.java
+++ FileIOExample.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.file.*;
 
 /**
 * This is a simple example of writing to and reading from a text file.
@@ -7,7 +8,8 @@ import java.io.*;
 public class FileIOExample {
 
     public static void main(String[] args) {
-
+            writeHighScore(42);
+            System.out.println(getHighScore());
 
     }
 
@@ -17,7 +19,7 @@ public class FileIOExample {
 
         Path file = FileSystems.getDefault().getPath("MyFile.txt");
 
-        // This is a try-with-resources statement.  It is available in Java 7 
+        // This is a try-with-resources statement.  It is available in Java 7
         // and later.  It will automatically close the Writer, so there is no
         // need for a finally clause, which would need additional Exception
         // handling
@@ -45,6 +47,30 @@ public class FileIOExample {
         } catch (IOException e) {
             // Some type of error message
         }
+    }
+
+    public static void writeHighScore(int score) {
+
+        Path file = FileSystems.getDefault().getPath("MyFile.txt");
+        String str = Integer.toString(score);
+
+        try (BufferedWriter write = Files.newBufferedWriter(file)) {
+            write.write(str);
+        } catch (IOException e) {}
+    }
+
+    public static int getHighScore() {
+        Path file = FileSystems.getDefault().getPath("MyFile.txt");
+        int highScore = 0;
+        try (BufferedReader reader = Files.newBufferedReader(file)) {
+
+            highScore = Integer.parseInt(reader.readLine());
+
+        } catch (IOException e) {
+            // Some type of error message
+        }
+
+        return highScore;
     }
 
 }
